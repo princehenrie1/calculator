2a39e960d33215133863ddc69abcf6eacb1dfd52
henrie <henriemensah34@gmail.com>
2020-10-09 16:55:57 +0000

    first commit

---
+++ demo1.js
@@ -0,0 +1,17 @@
+// document.write("<h1>External File</h1>");
+
+// var a = 5;
+// var b = 8;
+// var c = 3;
+// var ans = ((a*c)+b-a);
+
+// document.write("Result Is:"+ans+"");
+
+// if  (ans>10)
+// {
+//     document.write(" YES");
+// }
+// else
+// {
+//     document.write(" NO")
+// }
